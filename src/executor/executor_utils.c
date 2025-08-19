637fe8435e2514296bca419ff86e0927d0e600d9
Mariia Khudonohova <mariia@example.com>
2025-08-19 09:44:35 +0200

    update

--- src/executor/executor_utils.c
+++ src/executor/executor_utils.c
@@ -6,11 +6,59 @@
 /*   By: makhudon <makhudon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 08:58:09 by makhudon          #+#    #+#             */
-/*   Updated: 2025/08/16 11:34:09 by makhudon         ###   ########.fr       */
+/*   Updated: 2025/08/19 09:44:08 by makhudon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+// int precheck_pipe_syntax(const char *line)
+// {
+//     int   i;
+//     char  quote;      /* current active quote char (' or ") or 0 if none */
+//     int   seen_token; /* have we seen a non-space token since last '|' ? */
+
+//     i = 0;
+//     quote = 0;
+//     seen_token = 0;
+//     while (line && line[i])
+//     {
+//         if ((line[i] == '\'' || line[i] == '"') && quote == 0)
+//         {
+//             quote = line[i];
+//             i++;
+//             continue;
+//         }
+//         if (quote && line[i] == quote)
+//         {
+//             quote = 0;
+//             i++;
+//             seen_token = 1;
+//             continue;
+//         }
+//         if (quote == 0 && line[i] == '|')
+//         {
+//             int j;
+
+//             /* leading '|', double '||', or trailing '|' */
+//             j = i + 1;
+//             while (line[j] == ' ' || line[j] == '\t')
+//                 j++;
+//             if (seen_token == 0 || line[j] == '|' || line[j] == '\0')
+//             {
+//                 ft_error(NULL, "syntax error near unexpected token `|'");
+//                 return (0);
+//             }
+//             seen_token = 0; /* expect a token after this pipe */
+//             i++;
+//             continue;
+//         }
+//         if (quote == 0 && line[i] != ' ' && line[i] != '\t')
+//             seen_token = 1;
+//         i++;
+//     }
+//     return (1);
+// }
 
 void free_execute_data(t_execute_data *data)
 {
