8fd3b4f5125bf788818273acc2a64baa02f0c7ac
Mariia Khudonohova <mariia@example.com>
2025-08-22 11:47:55 +0200

    fixed error message in redirection

--- src/redirection/redirection.c
+++ src/redirection/redirection.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   redirection.c                                      :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: tiyang <tiyang@student.42.fr>                +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2025/07/24 08:25:34 by makhudon      #+#    #+#                 */
-/*   Updated: 2025/08/18 12:30:35 by tiyang        ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   redirection.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makhudon <makhudon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 08:25:34 by makhudon          #+#    #+#             */
+/*   Updated: 2025/08/22 11:40:23 by makhudon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,41 +30,101 @@ void	redirect_io(char *input_file, char *output_file, int output_mode)
 }
 
 // handle_redirection() Helper: Process a single redirection and update input/output file pointers
+// static int	process_redirection_token(char **args, int i, t_env_var *env_list, int last_exit_status,
+// 			char **final_input_file, char **final_output_file, int *output_mode, char **heredoc_file)
+// {
+//     if (!args[i + 1])
+//     {
+//         ft_putstr_fd("minishell: syntax error near `newline'\n", 2);
+//         return (-1);
+//     }
+// 	// KEY CHANGE HERE: Validate the token after the redirection operator.
+//     if (is_redirection(args[i + 1]))
+//     {
+//         // ft_putstr_fd("minishell: syntax error near unexpected token `", 2); // DEBUG: Print error if next token is also a redirection
+//         // ft_putstr_fd(args[i + 1], 2); // DEBUG: Print the next token
+//         // ft_putstr_fd("'\n", 2); // DEBUG: Print the closing quote
+// 		// KEY CHANGE HERE: Use ft_error to print the error message
+
+// 		char *error_msg_prefix = "syntax error near unexpected token `";
+//         char *error_msg_suffix = "'";
+//         char *full_msg = ft_strjoin(error_msg_prefix, args[i + 1]);
+//         char *final_msg = ft_strjoin(full_msg, error_msg_suffix);
+        
+//         ft_error("", final_msg);
+
+//         free(full_msg);
+//         free(final_msg);
+//         return (-1);
+//     }
+
+// 	if (ft_strncmp(args[i], "<<", 3) == 0)
+// 	{
+// 		if (*heredoc_file)
+// 		{
+// 			unlink(*heredoc_file);
+// 			free(*heredoc_file);
+// 			*heredoc_file = NULL; // Ensure it's set to NULL after freeing
+// 		}
+// 		*heredoc_file = handle_heredoc(args[i + 1], env_list, last_exit_status);
+// 		if (!*heredoc_file)
+// 			return (-1);
+// 		free(*final_input_file);
+// 		*final_input_file = ft_strdup(*heredoc_file);
+// 	}
+//     else if (ft_strncmp(args[i], "<", 2) == 0)
+//     {
+//         free(*final_input_file);
+//         *final_input_file = ft_strdup(args[i + 1]);
+//     }
+//     else if (ft_strncmp(args[i], ">", 2) == 0)
+//     {
+// 		if (process_output_file(args[i + 1], 0) != 0) // 0 = truncate
+//             return (-1);
+//         free(*final_output_file);
+//         *final_output_file = ft_strdup(args[i + 1]);
+// 		*output_mode = 1; // 1 = O_TRUNC
+//     }
+// 	else if (ft_strncmp(args[i], ">>", 3) == 0)
+// 	{
+// 		if (process_output_file(args[i + 1], 1) != 0) // 1 = append
+// 			return (-1);
+// 		free(*final_output_file);
+// 		*final_output_file = ft_strdup(args[i + 1]);
+// 		*output_mode = 2; // 2 = O_APPEND
+// 	}
+//     return (0);
+// }
+
 static int	process_redirection_token(char **args, int i, t_env_var *env_list, int last_exit_status,
 			char **final_input_file, char **final_output_file, int *output_mode, char **heredoc_file)
 {
-    if (!args[i + 1])
-    {
-        ft_putstr_fd("minishell: syntax error near `newline'\n", 2);
-        return (-1);
-    }
-	// KEY CHANGE HERE: Validate the token after the redirection operator.
-    if (is_redirection(args[i + 1]))
-    {
-        // ft_putstr_fd("minishell: syntax error near unexpected token `", 2); // DEBUG: Print error if next token is also a redirection
-        // ft_putstr_fd(args[i + 1], 2); // DEBUG: Print the next token
-        // ft_putstr_fd("'\n", 2); // DEBUG: Print the closing quote
-		// KEY CHANGE HERE: Use ft_error to print the error message
+	// 1️⃣ Make sure there is a token after the redirection operator
+	if (!args[i + 1])
+	{
+		ft_error("", "syntax error near unexpected token `newline'");
+		return (-1);
+	}
 
-		char *error_msg_prefix = "syntax error near unexpected token `";
-        char *error_msg_suffix = "'";
-        char *full_msg = ft_strjoin(error_msg_prefix, args[i + 1]);
-        char *final_msg = ft_strjoin(full_msg, error_msg_suffix);
-        
-        ft_error("", final_msg);
+	// 2️⃣ Check if the next token is also a redirection
+	if (is_redirection(args[i + 1]))
+	{
+		char *msg = ft_strjoin("syntax error near unexpected token `", args[i + 1]);
+		char *full_msg = ft_strjoin(msg, "'");
+		ft_error("", full_msg);
+		free(msg);
+		free(full_msg);
+		return (-1);
+	}
 
-        free(full_msg);
-        free(final_msg);
-        return (-1);
-    }
-
+	// 3️⃣ Handle heredoc
 	if (ft_strncmp(args[i], "<<", 3) == 0)
 	{
 		if (*heredoc_file)
 		{
 			unlink(*heredoc_file);
 			free(*heredoc_file);
-			*heredoc_file = NULL; // Ensure it's set to NULL after freeing
+			*heredoc_file = NULL;
 		}
 		*heredoc_file = handle_heredoc(args[i + 1], env_list, last_exit_status);
 		if (!*heredoc_file)
@@ -72,28 +132,42 @@ static int	process_redirection_token(char **args, int i, t_env_var *env_list, in
 		free(*final_input_file);
 		*final_input_file = ft_strdup(*heredoc_file);
 	}
-    else if (ft_strncmp(args[i], "<", 2) == 0)
-    {
-        free(*final_input_file);
-        *final_input_file = ft_strdup(args[i + 1]);
-    }
-    else if (ft_strncmp(args[i], ">", 2) == 0)
-    {
-		if (process_output_file(args[i + 1], 0) != 0) // 0 = truncate
-            return (-1);
-        free(*final_output_file);
-        *final_output_file = ft_strdup(args[i + 1]);
-		*output_mode = 1; // 1 = O_TRUNC
-    }
-	else if (ft_strncmp(args[i], ">>", 3) == 0)
+	// 4️⃣ Handle input redirection
+	else if (ft_strncmp(args[i], "<", 2) == 0)
 	{
-		if (process_output_file(args[i + 1], 1) != 0) // 1 = append
+		free(*final_input_file);
+		*final_input_file = ft_strdup(args[i + 1]);
+		if (access(*final_input_file, F_OK) != 0)
+		{
+			ft_error(args[i + 1], "No such file or directory");
+			return (-1);
+		}
+		if (access(*final_input_file, R_OK) != 0)
+		{
+			ft_error(args[i + 1], "Permission denied");
+			return (-1);
+		}
+	}
+	// 5️⃣ Handle output truncation
+	else if (ft_strncmp(args[i], ">", 2) == 0)
+	{
+		if (process_output_file(args[i + 1], 0) != 0)
 			return (-1);
 		free(*final_output_file);
 		*final_output_file = ft_strdup(args[i + 1]);
-		*output_mode = 2; // 2 = O_APPEND
+		*output_mode = 1; // O_TRUNC
 	}
-    return (0);
+	// 6️⃣ Handle output append
+	else if (ft_strncmp(args[i], ">>", 3) == 0)
+	{
+		if (process_output_file(args[i + 1], 1) != 0)
+			return (-1);
+		free(*final_output_file);
+		*final_output_file = ft_strdup(args[i + 1]);
+		*output_mode = 2; // O_APPEND
+	}
+
+	return (0);
 }
 
 // handle_redirection() Helper: Build clean argument array
