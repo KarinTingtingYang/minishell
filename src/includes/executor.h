6502bd132530a53a76d44a659971fbf64ddd4712
Tingting Yang <tiyang@f1r1s8.codam.nl>
2025-09-04 10:58:12 +0200

    cleaned up executor header

--- src/includes/executor.h
+++ src/includes/executor.h
@@ -6,88 +6,97 @@
 /*   By: tiyang <tiyang@student.42.fr>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/21 09:39:33 by makhudon      #+#    #+#                 */
-/*   Updated: 2025/09/04 10:05:21 by tiyang        ########   odam.nl         */
+/*   Updated: 2025/09/04 10:57:40 by tiyang        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTOR_H
-#define EXECUTOR_H
+# define EXECUTOR_H
 
-typedef struct s_command t_command;
+typedef struct s_command	t_command;
 
-typedef struct s_env_var t_env_var;
+typedef struct s_env_var	t_env_var;
 
 typedef struct s_execute_data
 {
-    char **original_args;  // The raw split arguments from the command line
-    char **clean_args;     // The arguments after removing redirection symbols and files
-    char *input_file;      // File name for input redirection (if any)
-    char *output_file;     // File name for output redirection (if any)
-    char **path_dirs;      // Array of directories in PATH environment variable
-    char *cmd_path;        // Full path to the executable command
-	int  output_mode;		//  FOR APPEND REDIRECTION: 0=none, 1=truncate(>), 2=append(>>)
-	t_env_var *env_list;
-	char *heredoc_file;    // File name for heredoc redirection (if any)
-	
-} t_execute_data;
+	char		**original_args;
+	char		**clean_args;
+	char		*input_file;
+	char		*output_file;
+	char		**path_dirs;
+	char		*cmd_path;
+	int			output_mode;
+	t_env_var	*env_list;
+	char		*heredoc_file;
+}	t_execute_data;
 
 // precheck_line.c
-int precheck_line(char *line, t_process_data *process_data);
+int			precheck_line(char *line, t_process_data *process_data);
 
 // precheck_line_helper.c
-int check_redir_error(const char *line, int op_len, int j,
-	t_process_data *process_data);
+int			check_redir_error(const char *line, int op_len, int j,
+				t_process_data *process_data);
 
 // path.c
-char *find_full_cmd_path(char *cmd, char **path_dirs);
-char **find_path_dirs(t_env_var *env_list);
+char		*find_full_cmd_path(char *cmd, char **path_dirs);
+char		**find_path_dirs(t_env_var *env_list);
 
 // executor.c
-int perform_command_checks(char *cmd_path, char **args, char **envp);
-void execute_cmd(char *cmd_path, char **args, char **path_dirs, t_env_var *env_list);
-int handle_pipeline_command(char *line, t_env_var *env_list, t_process_data *process_data);
-int handle_single_command(char *line, t_env_var *env_list, t_process_data *process_data);
-int execute_command(char *line, t_env_var *env_list, t_process_data *process_data);
+int			perform_command_checks(char *cmd_path, char **args, char **envp);
+void		execute_cmd(char *cmd_path, char **args, char **path_dirs,
+				t_env_var *env_list);
+int			handle_pipeline_command(char *line, t_env_var *env_list,
+				t_process_data *process_data);
+int			handle_single_command(char *line, t_env_var *env_list,
+				t_process_data *process_data);
+int			execute_command(char *line, t_env_var *env_list,
+				t_process_data *process_data);
 
 // execute_command_types.c
-int execute_builtin_command(t_execute_data *data, t_process_data *process_data);
-int execute_external_command(t_execute_data *data, t_process_data *process_data, t_env_var *env_list);
-int handle_redirection_only(t_execute_data *data, t_process_data *process_data);
-int execute_single_command(char **args, t_env_var *env_list, t_process_data *process_data);
+int			execute_builtin_command(t_execute_data *data,
+				t_process_data *process_data);
+int			execute_external_command(t_execute_data *data,
+				t_process_data *process_data, t_env_var *env_list);
+int			handle_redirection_only(t_execute_data *data,
+				t_process_data *process_data);
+int			execute_single_command(char **args, t_env_var *env_list,
+				t_process_data *process_data);
 
 // prepare_command_execution.c
-int prepare_command_execution(char *line, t_env_var *env_list, t_execute_data *data,
-								t_process_data *process_data);
+int			prepare_command_execution(char *line, t_env_var *env_list,
+				t_execute_data *data, t_process_data *process_data);
 
 // execute_prepared_command.c
-int execute_prepared_command(t_execute_data *data, t_process_data *process_data);
+int			execute_prepared_command(t_execute_data *data,
+				t_process_data *process_data);
 
 // prepare_pipeline_helper.c
-int build_commands_from_parts(char **parts, int count, t_process_data *data);
+int			build_commands_from_parts(char **parts, int count,
+				t_process_data *data);
 
 // prepare_pipeline_commands.c
-t_command **prepare_pipeline_commands(char *line, int *count, char ***parts,
-                                      t_process_data *process_data);
+t_command	**prepare_pipeline_commands(char *line, int *count, char ***parts,
+				t_process_data *process_data);
 
 // executor_helper.c
-int		validate_pipeline_parts(char **parts, int count);
-int		is_empty_or_whitespace(const char *str);
-char	**ft_split_dup(char **args);
-int		count_command_parts(char **parts);
-int		is_unquoted_pipe_present(const char *line);
+int			validate_pipeline_parts(char **parts, int count);
+int			is_empty_or_whitespace(const char *str);
+char		**ft_split_dup(char **args);
+int			count_command_parts(char **parts);
+int			is_unquoted_pipe_present(const char *line);
 
 // executor_error.c
-void handle_stat_error(char **envp, char **args, int error_code);
-void handle_access_error(char **envp, char **args, int error_code);
-void handle_execve_error(char **envp, char **args, int error_code);
-void handle_null_cmd_path(char **args, char **envp);
-int handle_redirection_error(t_execute_data *data, t_process_data *process_data,
-							char **args);
+void		handle_stat_error(char **envp, char **args, int error_code);
+void		handle_access_error(char **envp, char **args, int error_code);
+void		handle_execve_error(char **envp, char **args, int error_code);
+void		handle_null_cmd_path(char **args, char **envp);
+int			handle_redirection_error(t_execute_data *data,
+				t_process_data *process_data, char **args);
 
 // executor_cleanup.c
-void free_execute_data(t_execute_data *data);
-void free_commands_recursive(t_command **cmds, int index, int count);
-void cleanup_pipeline_resources(t_command **cmds, char **parts, 
-                                     char **path_dirs, int count);
+void		free_execute_data(t_execute_data *data);
+void		free_commands_recursive(t_command **cmds, int index, int count);
+void		cleanup_pipeline_resources(t_command **cmds, char **parts,
+				char **path_dirs, int count);
 
 #endif
